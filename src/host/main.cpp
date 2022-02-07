ab02cfa8494a7c87faff73ef1290a34f7c0f4f2e
Sebastian James <sebaxj@stanford.edu>
2022-02-07 08:08:50 -0800

    mac build compiles

--- src/host/main.cpp
+++ src/host/main.cpp
@@ -1,6 +1,4 @@
 /*
- * main.cpp
- */
 
 #include "RtAudio/RtAudio.h"
 #include "chuck.h"
@@ -16,7 +14,7 @@ using namespace std;
 // sample rate
 #define MY_SRATE 44100
 // number of channels
-#define MY_CHANNELS 2
+#define MY_CHANNELS 1
 // for convenience
 #define MY_PIE 3.14159265358979
 
@@ -124,6 +122,159 @@ int main(int argc, char **argv) {
   the_chuck->compileFile("test.ck", "");
 
   // start streaming
+  // go for it
+  try {
+    // start stream
+    audio.startStream();
+
+    // stop the stream.
+    audio.stopStream();
+  } catch (RtError &e) {
+    // print error message
+    cout << e.getMessage() << endl;
+    goto cleanup;
+  }
+
+cleanup:
+  // close if open
+  if (audio.isStreamOpen())
+    audio.closeStream();
+
+  // done
+  return 0;
+}
+*/
+//-----------------------------------------------------------------------------
+// name: VisualSine.cpp
+// desc: hello sine wave, real-time
+//
+// author: Ge Wang (ge@ccrma.stanford.edu)
+//   date: fall 2014
+//   uses: RtAudio by Gary Scavone
+//-----------------------------------------------------------------------------
+#include "RtAudio/RtAudio.h"
+#include "chuck.h"
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+
+//-----------------------------------------------------------------------------
+// function prototypes
+//-----------------------------------------------------------------------------
+
+// number of channels
+#define CHUCK_MY_CHANNELS 1
+// for convenience
+#define MY_PIE 3.14159265358979
+// our datetype
+#define SAMPLE float
+// corresponding format for RtAudio
+#define MY_FORMAT RTAUDIO_FLOAT32
+// sample rate
+#define CHUCK_MY_SRATE 44100
+// number of channels
+#define MY_CHANNELS_IN 1
+#define MY_CHANNELS_OUT 2
+#define GREATER(a, b) (a > b ? a : b)
+// for convenience
+#define MY_PIE 3.14159265358979
+
+ChucK *the_chuck = NULL;
+
+//-----------------------------------------------------------------------------
+// name: callme()
+// desc: audio callback
+//-----------------------------------------------------------------------------
+int callme(void *outputBuffer, void *inputBuffer, unsigned int numFrames,
+           double streamTime, RtAudioStreamStatus status, void *data) {
+  cout << "TEST 4" << endl;
+  // cast!
+  SAMPLE *input = (SAMPLE *)inputBuffer;
+  SAMPLE *output = (SAMPLE *)outputBuffer;
+
+  // compute chuck! Audio callback
+  the_chuck->run(input, output, numFrames);
+
+  // mute output -- TODO will need to disable this once ChucK produces output,
+  // in order for you to hear it!
+  //    for (int j = 0; j < MY_CHANNELS_OUT; j++) {
+  //     output[i * MY_CHANNELS_OUT + j] = 0;
+  //   }
+  cout << "END OF CALLBACK" << endl;
+
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+// name: main()
+// desc: entry point
+//-----------------------------------------------------------------------------
+int main(int argc, char **argv) {
+  // instantiate RtAudio object
+  RtAudio audio;
+  // variables
+  unsigned int bufferBytes = 0;
+  // frame size
+  unsigned int bufferFrames = 1024;
+
+  // check for audio devices
+  if (audio.getDeviceCount() < 1) {
+    // nopes
+    cout << "no audio devices found!" << endl;
+    exit(1);
+  }
+
+  // let RtAudio print messages to stderr.
+  audio.showWarnings(true);
+
+  // set input and output parameters
+  RtAudio::StreamParameters iParams, oParams;
+  iParams.deviceId = audio.getDefaultInputDevice();
+  iParams.nChannels = MY_CHANNELS_IN;
+  iParams.firstChannel = 0;
+  oParams.deviceId = audio.getDefaultOutputDevice();
+  oParams.nChannels = MY_CHANNELS_OUT;
+  oParams.firstChannel = 0;
+
+  // create stream options
+  RtAudio::StreamOptions options;
+
+  cout << "TEST 1" << endl;
+
+  // go for it
+  try {
+    // open a stream
+    audio.openStream(&oParams, &iParams, MY_FORMAT, CHUCK_MY_SRATE,
+                     &bufferFrames, &callme, (void *)&bufferBytes, &options);
+  } catch (RtError &e) {
+    // error!
+    cout << e.getMessage() << endl;
+    exit(1);
+  }
+  cout << "TEST 2" << endl;
+
+  // compute
+  bufferBytes =
+      bufferFrames * GREATER(MY_CHANNELS_OUT, MY_CHANNELS_IN) * sizeof(SAMPLE);
+
+  // set up chuck
+  the_chuck = new ChucK();
+  the_chuck->setParam(CHUCK_PARAM_SAMPLE_RATE,
+                      CHUCK_MY_SRATE); // set sample rate
+  the_chuck->setParam(CHUCK_PARAM_OUTPUT_CHANNELS,
+                      CHUCK_MY_CHANNELS); // set number of channels in and out
+  the_chuck->setParam(CHUCK_PARAM_INPUT_CHANNELS, CHUCK_MY_CHANNELS);
+  the_chuck->init();
+  the_chuck->compileFile("/Users/sebastianjames/src/project-031421/src/test.ck",
+                         "");
+  cout << "TEST 3" << endl;
+  // TODO: set sample rate and number of in/out channels on our chuck
+
+  // TODO: initialize our chuck
+
+  // TODO: run a chuck program
+
   // go for it
   try {
     // start stream
--- src/test.ck
+++ src/test.ck
@@ -1,32 +1,5 @@
-// sine oscillator as drone for A(69) and C#(73)
-SinOsc a => JCRev r1 => dac;
-SinOsc c => r1 => dac;
+SinOsc s => dac;
+1 => s.gain;
+440 => s.freq;
 
-// "a" frequency to 440 Hz
-Std.mtof(69) => a.freq;
-Std.mtof(73) => c.freq;
-
-// SinOsc amplitude
-.04 => a.gain;
-.02 => c.gain;
-
-// set mix of reverb to original signal
-.7 => r1.mix;
-
-// arrray to store connosance values
-[0, 2, 4, 7, 9, 11] @=> int arr[];
-
-// impulse UGen to generate string "plucks"
-Impulse imp => Delay str1 => dac;
-str1 => str1;
-
-0.98 => str1.gain;
-
-// infinite loop to execute plucks
-while(true)
-{
-        Std.mtof(57 + Math.random2(0,3) * 12 +
-        arr[Math.random2(0,arr.size()-1)])::samp => str1.delay;
-        1.0 => imp.next;
-        0.5::second => now;
-}
+while(true) 10::ms => now; 
